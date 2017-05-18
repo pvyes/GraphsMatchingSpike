7b97864827b5855711dafe744e9ddbae04507da4
Peter Vansweevelt <vs_peter@hotmail.com>
2017-05-18 10:37:25 +0200

    Revert "conditions added - not working yet!"
    This reverts commit 023a3352f3dda20567b4179823be7efcd2957eab.

--- src/test/java/nl/ou/abi/GraphsMatchingSpike/DesignPatternTest.java
+++ src/test/java/nl/ou/abi/GraphsMatchingSpike/DesignPatternTest.java
@@ -28,38 +28,30 @@ public class DesignPatternTest {
 		dp.addVertex(rightNode);
 		assertTrue(dp.containsVertex(rightNode));
 		assertFalse(dp.containsVertex(leftNode));
-		//addVertex returns false if vertex exists already in the graph
+		//addVertex is false when vertex exists already
 		assertFalse(dp.addVertex(rightNode));
 		
-		//add a second vertex
 		dp.addVertex(leftNode);
 		assertEquals(2, dp.vertexSet().size());
 		
 		//addEdge
 		Edge edge1 = dp.addEdge(leftNode, rightNode);
 		edge1.setName("edge1");
+		Node n = dp.getEdgeSource(edge1);
 		assertEquals(leftNode, dp.getEdgeSource(edge1));
 		assertEquals(1, dp.edgeSet().size());
-		
 		//adding an existing edge returns null (multiple edges are not allowed)
 		assertFalse(dp.isAllowingMultipleEdges());
 		assertNull(dp.addEdge(leftNode, rightNode));
-		
 		//change vertices of an existing edge by removing the edge from the graph, adding the new vertex (if necessary)
-		//and creating an edge in the graph with the existing edge as parameter
+		//and creating an edge in the graph with the existing edge
 		dp.removeEdge(edge1);
 		dp.addVertex(rightNode2);
 		dp.addEdge(leftNode, rightNode2, edge1);
 		assertEquals(rightNode2, dp.getEdgeTarget(edge1));
-		
-		//addEdge with an edge-parameter returns false if the Edge exists in the graph...
-		assertFalse(dp.addEdge(rightNode2, leftNode, edge1));
-		//... even if other vertices are involved (An existing Edge cannot change vertices, without removing the edge from the graph first)
-		assertFalse(dp.addEdge(rightNode, leftNode, edge1));		
 	}
 	
-	@Test
-	public void DesignPatternAdapterTester() {
+	private DesignPattern createAdapterPattern() {
 		Node client = new Clazz("Client", "Client");
 		Node target = new Interface("Target", "Target");
 		Node adapter = new Clazz("Adapter", "Adapter");
@@ -74,32 +66,9 @@ public class DesignPatternTest {
 		Edge clientTarget = dp.addEdge(client, target);
 		clientTarget.setName("clientTarget");
 		Edge adapterTarget = dp.addEdge(adapter, target);
-		Edge adapterAdaptee = dp.addEdge(adapter, adaptee);
+		Edge adpaterAdaptee = dp.addEdge(adapter, adaptee);
 		
-		assertEquals(4, dp.vertexSet().size());
-		assertEquals(3, dp.edgeSet().size());
-		assertEquals(1, dp.edgesOf(client).size());
-		assertEquals(2, dp.edgesOf(target).size());
-		assertEquals(2, dp.edgesOf(adapter).size());
-		assertEquals(1, dp.edgesOf(adaptee).size());
-		
-		assertEquals(adapter, dp.getEdgeSource(adapterAdaptee));
-		assertEquals(adaptee, dp.getEdgeTarget(adapterAdaptee));
-		assertEquals(adapter, dp.getEdgeSource(adapterTarget));
-		assertEquals(target, dp.getEdgeTarget(adapterTarget));
-		assertEquals(client, dp.getEdgeSource(clientTarget));
-		assertEquals(target, dp.getEdgeTarget(clientTarget));
-		
-		assertEquals(1, dp.outgoingEdgesOf(client).size());
-		assertEquals(0, dp.outgoingEdgesOf(target).size());
-		assertEquals(2, dp.outgoingEdgesOf(adapter).size());
-		assertEquals(0, dp.outgoingEdgesOf(adaptee).size());
-		
-		assertEquals(0, dp.incomingEdgesOf(client).size());
-		assertEquals(2, dp.incomingEdgesOf(target).size());
-		assertEquals(0, dp.incomingEdgesOf(adapter).size());
-		assertEquals(1, dp.incomingEdgesOf(adaptee).size());			
+		return dp;		
 	}
-
 
 }
